6a515b11852df542ee5b9255dbdeba63771e18c5
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-23 08:04:42 -0700

    chore: update

--- src/libs/timeline/augment.ts
+++ src/libs/timeline/augment.ts
@@ -1,19 +1,17 @@
 import type { Timeline } from "@prisma/client";
 
-import { bulkRead } from "@lightdotso/api/libs/cf/bulk";
+import { upstashRest } from "@lightdotso/api/libs/upstash";
 
 export const augmentTimeline = async (timeline: Timeline[]) => {
   const cmd = ["MGET"];
   for (const item of timeline) {
     cmd.push(`${item.type.toLowerCase()}:::${item.chainId}:::${item.id}`);
   }
-  const res = await bulkRead(cmd);
-  const json = (await res.json()) as string;
-  const data = JSON.stringify(json);
+  const data = await upstashRest(cmd);
 
   const result = timeline.map((item, index) => {
     let dict = {};
-    dict[item.type.toLowerCase()] = JSON.parse(data)[index];
+    dict[item.type.toLowerCase()] = JSON.parse(data.result[index]);
     return Object.assign({}, item, dict);
   });
 
