3674d7e26d5538ac4b052d0e335457ed958c157f
Shun Kakinoki <shunkakinoki@gmail.com>
2022-06-23 06:36:13 -0700

    chore: update

--- src/libs/cf/bulk.ts
+++ src/libs/cf/bulk.ts
@@ -1,3 +1,9 @@
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+
+export const isProduction = publicRuntimeConfig.env === "production";
+
 export const bulkWrite = async (data: any): Promise<Response> => {
   const CF_BULK_URL = `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.KV_NAMESPACE_ID}/bulk`;
 
@@ -13,9 +19,11 @@ export const bulkWrite = async (data: any): Promise<Response> => {
 };
 
 export const bulkRead = async (keys: string[]) => {
-  const CF_BULK_URL = `https://kv-preview.lightdotso.workers.dev/${keys.join(
-    ",",
-  )}`;
+  const CF_BULK_URL = `${
+    isProduction
+      ? "https://kv.lightdotso.workers.dev"
+      : "https://kv-preview.lightdotso.workers.dev"
+  }/${keys.join(",")}`;
 
   return fetch(CF_BULK_URL, {
     method: "GET",
