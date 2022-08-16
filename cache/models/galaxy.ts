60edbaf768064dc263beaa45ff0e6dc520f7ee41
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-15 18:43:29 -0700

    chore: update

--- cache/models/galaxy.ts
+++ cache/models/galaxy.ts
@@ -14,7 +14,7 @@ export const fetchGalaxyCampaign = (oatId: string) => {
     body: JSON.stringify({
       query: GALAXY_CAMPAIGN_QUERY,
       variables: {
-        oatId: oatId,
+        id: oatId,
       },
     }),
   });
