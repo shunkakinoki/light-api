1110ef8ef9d2e1c3b9301a94e7c70bad9f507f12
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-15 17:54:43 -0700

    chore: update

--- cache/models/galaxy.ts
+++ cache/models/galaxy.ts
@@ -1,5 +1,25 @@
 import { ApiLinks } from "@lightdotso/const";
-import { GALAXY_OAT_QUERY } from "@lightdotso/queries";
+import { GALAXY_OAT_QUERY, GALAXY_CAMPAIGN_QUERY} from "@lightdotso/queries";
+
+export const fetchGalaxyCampaign = (oatId: string) => {
+  return fetch(ApiLinks.GALAXY, {
+    method: "POST",
+    cf: {
+      cacheTtl: 300,
+      cacheEverything: true,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: GALAXY_CAMPAIGN_QUERY,
+      variables: {
+        oatId: oatId,
+      },
+    }),
+  });
+};
+
 
 export const fetchGalaxyOats = (address: string) => {
   return fetch(ApiLinks.GALAXY, {
--- cache/routes/galaxy.ts
+++ cache/routes/galaxy.ts
@@ -2,8 +2,18 @@
 
 import { compose } from "worktop";
 
-import { fetchGalaxyOats } from "../models/galaxy";
+import { fetchGalaxyCampaign, fetchGalaxyOats } from "../models/galaxy";
 import { addressValidator } from "../utils/validator";
+
+export const get = compose(undefined, async (req, context) => {
+  const oatId = context.params.oatId as string;
+
+  const result = await fetchGalaxyCampaign(oatId);
+  const res = new Response(result.body, result);
+  res.headers.set("Cache-Control", "max-age=300");
+
+  return res;
+});
 
 export const list = compose(addressValidator, async (req, context) => {
   const address = context.params.address as string;
