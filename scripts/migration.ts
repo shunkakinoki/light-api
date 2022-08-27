5d358b7680c29bd5f212035be57e62cb6fdfe637
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-26 19:58:01 -0700

    fix: ini migration

--- scripts/migration.ts
+++ scripts/migration.ts
@@ -1,5 +1,6 @@
 import { Redis } from "@upstash/redis";
 import * as dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 dotenv.config({ path: ".env.local" });
 
@@ -8,7 +9,11 @@ const redis = new Redis({
   token: process.env.UPSTASH_REST_API_TOKEN,
 });
 
+const client = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = client.connect();
+
 (async () => {
+  const client = await clientPromise;
   let cursor: number = 0;
   try {
     do {
@@ -16,12 +21,31 @@ const redis = new Redis({
         match: `alchemy:::1:::*`,
         count: 10,
       });
+      const promises = data[1].map(key => {
+        return redis.get(key);
+      });
+      const result = await Promise.all(promises);
+      const bulk = result.map(res => {
+        return {
+          insertOne: {
+            document: { chainId: 1, ...Object.assign({}, res) },
+          },
+        };
+      });
+      // try {
+      //   client.db("activity").collection("alchemy").bulkWrite(bulk);
+      // } catch (err) {
+      //   console.error(err);
+      // }
+      // eslint-disable-next-line no-console
+      console.log(data);
       if (data[1].length === 10) {
         cursor += 10;
       } else {
         cursor = -1;
       }
-      console.log(data);
+      // eslint-disable-next-line no-console
+      console.log(cursor);
     } while (cursor > 0);
   } catch (error) {
     console.error(error);
