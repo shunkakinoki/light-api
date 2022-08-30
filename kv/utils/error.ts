46f3872dd99a4d7085f73f42db421a74584caaa9
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-29 23:47:58 -0700

    chore: update

--- kv/utils/error.ts
+++
@@ -1,8 +0,0 @@
-import * as res from "worktop/response";
-
-import { response } from "./response";
-
-export const toError = (code: number, error?: Error): Response => {
-  let body = (error && error.message) || res.STATUS_CODES[code];
-  return response(code, body || String(code));
-};
--- kv/utils/response.ts
+++
@@ -1,8 +0,0 @@
-import * as res from "worktop/response";
-
-export const response: typeof res.reply = (code, data, headers) => {
-  if (code >= 400 && typeof data === "string") {
-    data = { status: code, error: data };
-  }
-  return res.reply(code, data, headers);
-};
