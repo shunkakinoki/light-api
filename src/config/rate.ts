b01b2a0a38208a1641e6619a75aec4fb6e302337
Shun Kakinoki <shunkakinoki@gmail.com>
2022-08-29 22:40:28 -0700

    perf: remove api module

--- src/config/rate.ts
+++
@@ -1,2 +0,0 @@
-export const RATE_LIMIT = 30;
-export const RATE_TIMEFRAME = 30;
--- src/config/upstash.ts
+++
@@ -1,9 +0,0 @@
-export const Hashes = ["API_KEYS", "FOUR_BYTE", "IP_RULES"] as const;
-
-export const Upstash: {
-  readonly [key in typeof Hashes[number]]: string;
-} = {
-  API_KEYS: "api:keys",
-  FOUR_BYTE: "fourbyte",
-  IP_RULES: "ip:rules",
-};
